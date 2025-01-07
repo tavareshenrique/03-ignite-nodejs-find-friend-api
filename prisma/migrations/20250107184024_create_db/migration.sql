0b7a28739b461fffb284eb7d58dd3384fd1c30f1
Henrique Tavares <hdptavares@rd.com.br>
2025-01-07 15:43:32 -0300

    feat: set up PostgreSQL database schema and update Docker Compose service name

---
+++ prisma/migrations/20250107184024_create_db/migration.sql
@@ -0,0 +1,55 @@
+-- CreateEnum
+CREATE TYPE "Levels" AS ENUM ('SMALL', 'MEDIUM', 'LARGE');
+
+-- CreateEnum
+CREATE TYPE "PetAge" AS ENUM ('PUPPY', 'ADULT', 'ELDERLY');
+
+-- CreateEnum
+CREATE TYPE "PetEnvironment" AS ENUM ('APARTMENT', 'HOUSE_WITH_YARD', 'LARGE_ENVIRONMENT');
+
+-- CreateTable
+CREATE TABLE "photos" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "pet_id" TEXT,
+
+    CONSTRAINT "photos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "breed" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "age" "PetAge" NOT NULL,
+    "size" "Levels" NOT NULL,
+    "independence_level" "Levels" NOT NULL,
+    "energy_level" "Levels" NOT NULL,
+    "environment" "PetEnvironment" NOT NULL,
+    "adoption_requirements" TEXT[],
+    "organization_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organizations" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "owner" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "zipcode" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "photos" ADD CONSTRAINT "photos_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
