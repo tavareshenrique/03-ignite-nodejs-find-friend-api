01f31e9836794375f7ed0e1b836e88c9645020a0
Henrique Tavares <hdptavares@rd.com.br>
2025-01-08 14:38:47 -0300

    feat: initialize project structure with environment configuration and server setup

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import fastifyCookie from '@fastify/cookie'
+import fastifyJwt from '@fastify/jwt'
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+
+import { env } from './env'
+
+export const app = fastify()
+
+app.register(fastifyJwt, {
+	secret: env.JWT_SECRET,
+	cookie: {
+		cookieName: 'refreshToken',
+		signed: false,
+	},
+	sign: {
+		expiresIn: '10m',
+	},
+})
+
+app.register(fastifyCookie)
+
+app.setErrorHandler((error, _, reply) => {
+	if (error instanceof ZodError) {
+		return reply.status(400).send({
+			message: 'Validation error.',
+			issues: error.format(),
+		})
+	}
+
+	if (env.NODE_ENV !== 'production') {
+		console.error(error)
+	} else {
+		// TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry/Airbrake
+	}
+
+	return reply.status(500).send({
+		message: 'Internal server error.',
+	})
+})
