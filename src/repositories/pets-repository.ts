c30657082aa58c7d8e3565c1644c327cdc73976d
Henrique Tavares <hdptavares@rd.com.br>
2025-01-08 14:55:26 -0300

    feat: add PetRepository interface for managing pet data retrieval

---
+++ src/repositories/pets-repository.ts
@@ -0,0 +1,14 @@
+import { Levels, Pet, PetAge, PetEnvironment } from '@prisma/client'
+
+export interface PetRepository {
+	findById(id: string): Promise<Pet | null>
+	findMany(
+		city: string,
+		breed?: string,
+		age?: PetAge,
+		size?: Levels,
+		independence_level?: Levels,
+		energy_level?: Levels,
+		environment?: PetEnvironment,
+	): Promise<Pet[] | null>
+}
