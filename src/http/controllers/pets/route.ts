171b191521ecac81330b8231deadec1ae743d421
Henrique Tavares <hdptavares@rd.com.br>
2025-01-09 17:32:49 -0300

    fix: change pet route from POST to GET for retrieving pet details

--- src/http/controllers/pets/route.ts
+++ src/http/controllers/pets/route.ts
@@ -3,5 +3,5 @@ import { FastifyInstance } from 'fastify'
 import { get } from './get'
 
 export async function petsRoutes(app: FastifyInstance) {
-	app.post('/pets/:petId', get)
+	app.get('/pets/:petId', get)
 }
