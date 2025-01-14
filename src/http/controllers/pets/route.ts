1e5c6855262aa9f602b4efe7a11e1017f6647426
Henrique Tavares <ihenrits@gmail.com>
2025-01-14 20:04:49 -0300

    feat: add POST route for creating new pets in petsRoutes

--- src/http/controllers/pets/route.ts
+++ src/http/controllers/pets/route.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from 'fastify'
 
+import { create } from './create'
 import { get } from './get'
 
 export async function petsRoutes(app: FastifyInstance) {
 	app.get('/pets/:petId', get)
+
+	app.post('/pets', create)
 }
