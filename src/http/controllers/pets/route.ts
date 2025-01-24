17e12b9b4362fc2df29cad5fb400f47f2e900f96
Henrique Tavares <ihenrits@gmail.com>
2025-01-24 15:53:23 -0300

    feat: add JWT authentication to pets creation route

--- src/http/controllers/pets/route.ts
+++ src/http/controllers/pets/route.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
 import { create } from './create'
 import { get } from './get'
 import { getMany } from './get-many'
@@ -8,5 +10,6 @@ export async function petsRoutes(app: FastifyInstance) {
 	app.get('/pets/:petId', get)
 	app.get('/pets', getMany)
 
-	app.post('/pets', create)
+	/** Authenticated Bellow */
+	app.post('/pets', { onRequest: [verifyJWT] }, create)
 }
