08cbd0d42cf6d6c07c795f9a5c27683dfb108041
Henrique Tavares <ihenrits@gmail.com>
2025-01-30 14:19:21 -0300

    feat: add refresh token endpoint for JWT management

--- src/http/controllers/organizations/route.ts
+++ src/http/controllers/organizations/route.ts
@@ -2,8 +2,11 @@ import { FastifyInstance } from 'fastify'
 
 import { authenticate } from './authenticate'
 import { create } from './create'
+import { refresh } from './refresh'
 
 export async function organizationsRoutes(app: FastifyInstance) {
 	app.post('/organizations', create)
 	app.post('/sessions', authenticate)
+
+	app.patch('/token/refresh', refresh)
 }
