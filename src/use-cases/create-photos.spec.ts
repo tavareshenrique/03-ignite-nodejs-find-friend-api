89ce76f24f45eaab90e7e26284c9054a15fe6176
Henrique Tavares <ihenrits@gmail.com>
2025-01-31 15:08:04 -0300

    feat: add unit tests for CreatePhotosUseCase with in-memory photos repository

---
+++ src/use-cases/create-photos.spec.ts
@@ -0,0 +1,29 @@
+import { faker } from '@faker-js/faker'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryPhotosRepository } from '@/repositories/in-memory/in-memory-photos-repository'
+
+import { CreatePhotosUseCase } from './create-photos'
+
+let organizationsRepository: InMemoryPhotosRepository
+let sut: CreatePhotosUseCase
+
+describe('Create Photos Case', () => {
+	beforeEach(() => {
+		organizationsRepository = new InMemoryPhotosRepository()
+
+		sut = new CreatePhotosUseCase(organizationsRepository)
+	})
+
+	it('should be able to add photos', async () => {
+		const petId = faker.string.uuid()
+		const photos = [faker.internet.url(), faker.internet.url()]
+
+		const response = await sut.execute({
+			petId,
+			photos,
+		})
+
+		expect(response.photos).toHaveLength(2)
+	})
+})
